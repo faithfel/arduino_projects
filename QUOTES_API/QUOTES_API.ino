53b43f6056f6b79a7738d37d5cf19fb38026bfa2
Faith <felominofaith@gmail.com>
2026-04-07 19:16:18 +0800

    Update QUOTES_API.ino

--- QUOTES_API/QUOTES_API.ino
+++ QUOTES_API/QUOTES_API.ino
@@ -1,42 +1,42 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include <ArduinoJson.h>
+    #include <WiFi.h>
+    #include <HTTPClient.h>
+    #include <ArduinoJson.h>
 
 
-//chnage pass below
-const char* ssid = "ssid";
-const char* password = "pass";
+    //chnage pass below
+    const char* ssid = "ssid";
+    const char* password = "pass";
 
-void setup(){
-    Serial.begin(115200);
-    delay(1000);
+    void setup(){
+        Serial.begin(115200);
+        delay(1000);
 
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    Serial.println("\nConnecting....");
+        WiFi.mode(WIFI_STA);
+        WiFi.begin(ssid, password);
+        Serial.println("\nConnecting....");
 
-    while(WiFi.status() != WL_CONNECTED){
-        Serial.print(".");
-        delay(500);
+        while(WiFi.status() != WL_CONNECTED){
+            Serial.print(".");
+            delay(500);
+        }
+
+        Serial.println("\nConnected to the WiFi network!!!");
+        Serial.print("Local ESP32 IP: ");
+        Serial.println(WiFi.localIP());
     }
 
-    Serial.println("\nConnected to the WiFi network!!!");
-    Serial.print("Local ESP32 IP: ");
-    Serial.println(WiFi.localIP());
-}
+    void loop(){
+        if ((WiFi.status() == WL_CONNECTED)){
+            long rnd = random(1, 50);
+            HTTPClient client;
 
-void loop(){
-    if ((WiFi.status() == WL_CONNECTED)){
-        long rnd = random(1, 50);
-        HTTPClient client;
+            client.begin("https://zenquotes.io/api/quotes" + String (rnd));
+            int httpCode = client.GET();
 
-        client.begin("https://zenquotes.io/api/quotes" + String (rnd));
-        int httpCode = client.GET();
-
-        if (httpCode > 0) {
-            String payload = client.getString();
-            Serial.println("\n Statuscode" + String(httpCode));
-            Serial.println(payload);
+            if (httpCode > 0) {
+                String payload = client.getString();
+                Serial.println("\n Statuscode" + String(httpCode));
+                Serial.println(payload);
 
         }
         else {
